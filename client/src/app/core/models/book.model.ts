b607e15d92f26718686e0ac39985821685c899dd
Gaurav Mishra <gauravanim@gmail.com>
2021-03-01 07:38:26 +0530

    Kitab Ghar - SPA demo

---
+++ client/src/app/core/models/book.model.ts
@@ -0,0 +1,24 @@
+import { Comment } from './comment.model';
+
+export class Book {
+  constructor(
+    public _id: string,
+    public title: string,
+    public author: string,
+    public publisher: string,
+    public genre: string,
+    public year: number,
+    public description: string,
+    public cover: string,
+    public isbn: string,
+    public pagesCount: number,
+    public price: number,
+    public qty?: number,
+    public creationDate?: Date,
+    public currentRating?: number,
+    public ratingPoints?: number,
+    public ratedCount?: number,
+    public purchasesCount?: number,
+    public comments?: Comment[]
+  ) { }
+}
---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        connectionString: 'mongodb+srv://admin:1234@cluster0.naqkf.mongodb.net/BookStore'
+    },
+    production: {
+
+    }
+};
+
+
